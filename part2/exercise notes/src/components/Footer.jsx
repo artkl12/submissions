42978bb2c15971070e94d3aac7b3b871370687c1
artkl12 <arta.klauze@gmail.com>
2024-10-05 13:04:42 +0300

    commit

---
+++ part2/exercise notes/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    sontSize: '16'
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, Univeristy of Helsinki 2024</em>      
+    </div>
+  )
+}
+
+export default Footer
