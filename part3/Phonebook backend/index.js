3ef7a296badf1a9c4e93c938c43f1cecd1b5dd5b
artkl12 <arta.klauze@gmail.com>
2024-10-22 13:59:37 +0300

    deploy

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -1,33 +1,34 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
 
-require('dotenv').config()
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
+app.use(express.static('dist'))
 app.use(express.json());
 morgan.token("body", function (request, response) {
   return JSON.stringify(request.body);
@@ -36,6 +37,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -60,21 +62,39 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+app.get("/api/persons/:id", (request, response, next) => {
+  // const id = request.params.id;
+  // const person = persons.find((person) => person.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
+
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }    
+  })
+  .catch(error => {
+    console.log(error)
+    next(error)
+  })
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  // const id = request.params.id;
+  // persons = persons.filter((person) => person.id !== id);
+  // response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
 const generateId = () => {
@@ -85,11 +105,10 @@ const generateId = () => {
     id = Math.floor(Math.random() * 100).toString();
     isUnique = !persons.some((person) => person.id === id);
   }
-
   return id;
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -105,15 +124,39 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
+    // id: generateId(),
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  // persons = persons.concat(person);
+  // response.json(person);
+
+  person.save()
+  .then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 });
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name, number} = request.body
+
+  // const person = {
+  //   name: body.name,
+  //   number: body.number,
+  // }
+
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    {name, number}, 
+    {new: true, runValidators: true, context: 'query'})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -121,36 +164,21 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const mongoose = require('mongoose')
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-mongoose.set('strictQuery', false)
-// const password = process.argv[2]
-
-
-const url = process.env.MONGODB_URI;
-console.log('connecting to ', url)
-mongoose.connect(url).then(result => {
-  console.log('connected to MONGODB')
-})
-.catch((error) => {
-  console.log('error connecting to MongoDB:', error.message)
-})
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
-})
 
-const PORT = process.env.PORT || 3002;
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
--- part3/phonebook frontend/src/components/Persons.jsx
+++ part3/phonebook frontend/src/components/Persons.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import Person from "./Person";
 
 const Persons = ({ persons, searchVal, handleDelete }) => {
+  console.log(persons)
   return (
     <div>
-      {persons
+      {/* {persons
         .filter((person) =>
           person.name.toLowerCase().includes(searchVal.toLowerCase())
         )
         .map((filteredPerson) => (
           <Person key={filteredPerson.name} person={filteredPerson} handleDelete={handleDelete} />
-        ))}
+        ))} */}
+        {
+          persons.map((filteredPerson) => (
+          <Person key={filteredPerson.name} person={filteredPerson} handleDelete={handleDelete} />
+        ))
+        }
     </div>
   );
 };
